a3f2bc878ece2de2ef22ca3b29d30c71b3194c99
fishfood65 <167575622+fishfood65@users.noreply.github.com>
2025-03-29 00:29:05 +0000

    start code

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,24 +5,29 @@ st.set_page_config(
     page_icon="👋",
 )
 
-st.write("# Welcome to Streamlit! 👋")
+st.write("# Welcome to Home Hero Academy! 👋")
 
 st.sidebar.success("Select a demo above.")
 
+
 st.markdown(
     """
-    Streamlit is an open-source app framework built specifically for
-    Machine Learning and Data Science projects.
-    **👈 Select a demo from the sidebar** to see some examples
-    of what Streamlit can do!
-    ### Want to learn more?
-    - Check out [streamlit.io](https://streamlit.io)
-    - Jump into our [documentation](https://docs.streamlit.io)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    ### Your Mission
+    Accept a series of challenges testing your knowledge about your home to empower you and your deputies to become a heroic guardian for your home and its precious contents
 """
 )
+
+st.markdown(
+    """
+    ### Start your Training!
+"""
+)
+# Display a button for the user to accept the mission
+if st.button("Accept Mission"):
+    st.write("You've accepted the mission! Let's get started. Please enter your city and zip code:")
+    city = st.text_input("Enter Your City and Zip Code:")
+    st.write(f"Thank you for entering {city}. We'll provide tailored emergency instructions for your area.")
+
+# If the user doesn't accept the mission, display a message
+else:
+    st.write("You haven't accepted the mission yet. Please click the button to begin your journey.")
